7b940376af7e8079676bd365022880ddea3ffbb3
c7harry <76096139+c7harry@users.noreply.github.com>
2025-06-06 02:36:30 -0700

    added theme toggle switch (uiverse), still working on functionalty

--- BayClock/src/App.jsx
+++ BayClock/src/App.jsx
@@ -14,11 +14,11 @@ function Layout({ children }) {
     return <>{children}</>;
   }
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen w-screen max-w-full overflow-x-hidden">
       <Sidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0">
         <Navbar />
-        <main className="p-6 overflow-y-auto">{children}</main>
+        <main className="p-6 overflow-y-auto min-w-0">{children}</main>
       </div>
     </div>
   );
